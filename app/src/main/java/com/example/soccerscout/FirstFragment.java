34670a75bbf124a00bf1a11e66d9478fe93c3f18
gonzarf <gonzarf21@gmail.com>
2023-11-22 14:23:48 +0100

    FloatingButton

--- app/src/main/java/com/example/soccerscout/FirstFragment.java
+++ app/src/main/java/com/example/soccerscout/FirstFragment.java
@@ -11,12 +11,10 @@ import android.view.ViewGroup;
 public class FirstFragment extends Fragment {
 
     public FirstFragment(){
-        // require a empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
 }
